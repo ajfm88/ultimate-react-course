dddbc7d7b2792e9458355ae38e1817f057d4e36f
ａｊｆｍ８８ <ale@ajfm88.com>
2026-09-23 20:24:01 -0700

    145. Using an async Function. 146. Adding a Loading State.

--- 07-usepopcorn-v2/src/App.js
+++ 07-usepopcorn-v2/src/App.js
@@ -54,11 +54,20 @@ const KEY = process.env.REACT_APP_KEY;
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const query = "interstellar";
 
   useEffect(function () {
-    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-      .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
+    async function fetchMovies() {
+      setIsLoading(true);
+      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s={query}`);
+      const data = await res.json();
+      setMovies(data.Search);
+      console.log(movies); // empty array, setting state is asynchronous
+      console.log(data.Search);
+      setIsLoading(false);
+    }
+    fetchMovies();
   }, []); // Empty array = will only run on mount.
 
   return (
@@ -69,9 +78,7 @@ export default function App() {
       </NavBar>
 
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
 
         <Box>
           <WatchedSummary watched={watched} />
@@ -80,6 +87,10 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="loader">Loading...</p>;
 }
 
 function NavBar({ children }) {
